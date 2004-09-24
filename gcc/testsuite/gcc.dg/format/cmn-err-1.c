f68e12504e0e25f914b87415e8b87bf19b1aef86
mmitchel <mmitchel@138bc75d-0d04-0410-961f-82ee72b054a4>
2004-09-24 20:23:28 +0000

    * gcc/config/i386/sol2-10.h (LIBGCC_SPEC): Add support for -nolibgcc.
    	* gcc/config/sol2-c.c (cmn_err_flag_specs): Add "#"
    	(cmn_err_char_table): Likewise.
    	(solaris_format_types): Likewise.
    	* gcc/testsuite/gcc.dg/format/cmn-err-1.c: Add "#" tests.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl-sol210-3_4-branch@88068 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/testsuite/gcc.dg/format/cmn-err-1.c
+++ gcc/testsuite/gcc.dg/format/cmn-err-1.c
@@ -17,6 +17,7 @@ int main()
   int i = 1;
   long l = 2;
   llong ll = 3;
+  float f = 4.0;
 
   cmn_err_func (0, "%s", string);
   cmn_err_func (0, "%d %D %o %O %x %X %u", i, i, i, i, i, i, i);
@@ -26,6 +27,7 @@ int main()
   cmn_err_func (0, "%b %s", i, "\01Foo", string);
   cmn_err_func (0, "%p", string);
   cmn_err_func (0, "%16b", i, "\01Foo");
+  cmn_err_func (0, "%#x", i);
 
   cmn_err_func (0, "%i", i);		/* { dg-error "unknown|too many" } */
   cmn_err_func (0, "%d", l);		/* { dg-error "expects type" } */
@@ -34,5 +36,6 @@ int main()
   cmn_err_func (0, "%b", i, i);		/* { dg-error "expects type" } */
   cmn_err_func (0, "%b", string, i);	/* { dg-error "expects type" } */
   cmn_err_func (0, "%p", 3);            /* { dg-error "expects type" } */
+  cmn_err_func (0, "%#x", f);           /* { dg-error "double arg" } */
   return 0;
 }
