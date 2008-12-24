566a4a6e4b9e53e1ea9052568937e779fda8cacf
Lijuan Hai <lijuan.hai@sun.com>
2008-12-24 02:26:33 -0800

    update golden files and testcases.

--- gcc/testsuite/g++.dg/ext/va-arg-pack-2.C
+++ gcc/testsuite/g++.dg/ext/va-arg-pack-2.C
@@ -1,5 +1,5 @@
 // { dg-do compile }
-// { dg-options "-O2" }
+// { dg-options "-O2 -Wd,-w" }
 
 int bar (int, const char *, int, ...);
 int baz (int, const char *, long int);
--- gcc/testsuite/g++.dg/ext/va-arg-pack-len-1.C
+++ gcc/testsuite/g++.dg/ext/va-arg-pack-len-1.C
@@ -1,5 +1,5 @@
 // { dg-do run }
-// { dg-options "-O2" }
+// { dg-options "-O2 -Wd,-w" }
 
 #include <stdarg.h>
 
