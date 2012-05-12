b31be9c79bb1b38072fda49b2ad0e2d2586267db
Richard Lowe <richlowe@richlowe.net>
2012-05-12 00:55:16 -0400

    g++spec: Remove more vestiges of runpath magic
    We used to add runpath entries, and needed to keep track of 64bit-ness
    to do this.  The runpath magic was removed, the tracking was not.

--- gcc/cp/g++spec.c
+++ gcc/cp/g++spec.c
@@ -60,9 +60,6 @@ lang_specific_driver (int *in_argc, const char *const **in_argv,
   /* If nonzero, the user gave us the `-v' flag.  */
   int saw_verbose_flag = 0;
 
-  /* If nonzero, the user gave us the '-m64' flag.  */
-  int saw_m64_flag = 0;
-
   /* This is a tristate:
      -1 means we should not link in libstdc++
      0  means we should link in libstdc++ if it is needed
@@ -208,10 +205,6 @@ lang_specific_driver (int *in_argc, const char *const **in_argv,
 	    shared_libgcc = 0;
 	  else if (DEFAULT_WORD_SWITCH_TAKES_ARG (&argv[i][1]))
 	    i++;
-	  else if (strcmp (argv[i], "-m64") == 0)
-	    saw_m64_flag = 1;
-	  else if (strcmp (argv[i], "-m32") == 0)
-	    saw_m64_flag = 0;
 	  else
 	    /* Pass other options through.  */
 	    continue;
