303fcce7d1c45aedda40723199919fc93a55bb7c
Richard Lowe <richlowe@richlowe.net>
2012-10-27 02:44:09 +0100

    Implement -fstrict-calling-conventions
    Stock GCC is overly willing to violate the ABI when calling local functions,
    such that it passes arguments in registers on i386.  This hampers debugging
    with anything other than a fully-aware DWARF debugger, and is generally not
    something we desire.
    
    Implement a flag which disables this behaviour, enabled by default.  The flag is
    global, though only effective on i386, to more easily allow its globalization
    later which, given the odds, is likely to be necessary.

--- gcc/config/i386/i386.c
+++ gcc/config/i386/i386.c
@@ -1117,6 +1117,7 @@ ix86_function_regparm (const_tree type, const_tree decl)
 	 and callee not, or vice versa.  Instead look at whether the callee
 	 is optimized or not.  */
       if (target && opt_for_fn (target->decl, optimize)
+	  && !flag_strict_calling_conventions
 	  && !(profile_flag && !flag_fentry))
 	{
 	  if (target->local && target->can_change_signature)
@@ -1213,6 +1214,7 @@ ix86_function_sseregparm (const_tree type, const_tree decl, bool warn)
       /* TARGET_SSE_MATH */
       && (target_opts_for_fn (target->decl)->x_ix86_fpmath & FPMATH_SSE)
       && opt_for_fn (target->decl, optimize)
+      && !flag_strict_calling_conventions
       && !(profile_flag && !flag_fentry))
     {
       if (target->local && target->can_change_signature)
