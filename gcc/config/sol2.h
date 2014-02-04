b9a3cdf8f21ba11e06ec0fd5d32526c821de7694
ro <ro@138bc75d-0d04-0410-961f-82ee72b054a4>
2014-02-04 09:31:38 +0000

    Ensure libgcc_s unwinder is always used on 64-bit Solaris 10+/x86 (PR target/59788)
    	gcc:
    	PR target/59788
    	* config/sol2.h (LINK_LIBGCC_MAPFILE_SPEC): Define.
    	(LINK_SPEC): Use it for -shared, -shared-libgcc.
    
    	libgcc:
    	PR target/59788
    	* config/t-slibgcc-sld (libgcc-unwind.map): New target.
    	(install-libgcc-unwind-map-forbuild): New target.
    	(all): Depend on install-libgcc-unwind-map-forbuild.
    	(install-libgcc-unwind-map): New target.
    	(install): Depend on install-libgcc-unwind-map.
    
    	gcc/testsuite:
    	PR target/59788
    	* g++.dg/eh/unwind-direct.C: New test.
    
    	toplevel:
    	PR target/59788
    	* ltmain.sh (opt_duplicate_compiler_generated_deps): Enable on
    	*solaris2*.
    
    git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/trunk@207454 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/sol2.h
+++ gcc/config/sol2.h
@@ -166,12 +166,21 @@ along with GCC; see the file COPYING3.  If not see
 #define RDYNAMIC_SPEC "--export-dynamic"
 #endif
 
+#ifndef USE_GLD
+/* With Sun ld, use mapfile to enforce direct binding to libgcc_s unwinder.  */
+#define LINK_LIBGCC_MAPFILE_SPEC \
+  "%{shared|shared-libgcc:-M %slibgcc-unwind.map}"
+#else
+/* GNU ld doesn't support direct binding.  */
+#define LINK_LIBGCC_MAPFILE_SPEC ""
+#endif
+
 #undef  LINK_SPEC
 #define LINK_SPEC \
   "%{h*} %{v:-V} \
    %{!shared:%{!static:%{rdynamic: " RDYNAMIC_SPEC "}}} \
    %{static:-dn -Bstatic} \
-   %{shared:-G -dy %{!mimpure-text:-z text}} \
+   %{shared:-G -dy %{!mimpure-text:-z text}} " LINK_LIBGCC_MAPFILE_SPEC " \
    %{symbolic:-Bsymbolic -G -dy -z text} \
    %{pthreads|pthread|fprofile-generate*:" LIB_THREAD_LDFLAGS_SPEC "} \
    %(link_arch) \
