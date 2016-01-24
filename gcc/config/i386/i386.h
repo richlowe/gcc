8deb3ac534407735dd49bee8c9239df21d6e1f1a
Richard Lowe <richlowe@richlowe.net>
2016-01-23 22:14:56 -0500

    i386: Save integer-passed arguments to the stack, to aid debuggers.
    Merged from 5.3 to 7.2 by: Aurélien Larcher
    
    Originally implemented in:
        commit 023cc9a4c9c698bed1f51d38eac850d327fc1146
        Author: jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
        Date:   Wed Jun 29 23:51:34 2005 +0000
    
            	* gcc/dwarf2.h (DW_AT_SUN_amd64_parmdump): New.
            	* gcc/dwarf2out.c (gen_subprogram_die): Add this attribute.
            	* gcc/doc/invoke.texi (-msave-args): New x86-64 option.
            	* gcc/config/i386/i386.h (MASK_SAVE_ARGS, TARGET_SAVE_ARGS): New.
            	(TARGET_SWITCHES): Add -msave-args.
            	* gcc/config/i386/i386.c (struct ix86_frame): Add nmsave_args and
            	padding0.
            	(pro_epilogue_adjust_stack): Declare.
            	(ix86_nsaved_args): New.
            	(override_options, ix86_can_use_return_insn_p,
            	ix86_frame_pointer_required, ix86_compute_frame_layout,
            	ix86_emit_save_regs, ix86_emit_save_regs_using_mov,
            	ix86_expand_prologue, ix86_expand_epilogue): Handle -msave-args.
    
            git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl-sol210-3_4-branch@101443 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.h
+++ gcc/config/i386/i386.h
@@ -2530,6 +2530,11 @@ enum avx_u128_state
 
    saved frame pointer			if frame_pointer_needed
 					<- HARD_FRAME_POINTER
+
+   [-msave-args]			<- arg_save_offset
+
+   [saveargs padding]
+
    [saved regs]
 					<- reg_save_offset
    [padding0]
@@ -2563,6 +2568,7 @@ enum avx_u128_state
   */
 struct GTY(()) ix86_frame
 {
+  int nmsave_args;
   int nsseregs;
   int nregs;
   int va_arg_size;
@@ -2574,6 +2580,7 @@ struct GTY(()) ix86_frame
   HOST_WIDE_INT hard_frame_pointer_offset;
   HOST_WIDE_INT stack_pointer_offset;
   HOST_WIDE_INT hfp_save_offset;
+  HOST_WIDE_INT arg_save_offset;
   HOST_WIDE_INT reg_save_offset;
   HOST_WIDE_INT stack_realign_allocate;
   HOST_WIDE_INT stack_realign_offset;
