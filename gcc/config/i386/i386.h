13728359e5b7fbbbe1c51f48f18cadfcf19914d5
Richard Lowe <richlowe@richlowe.net>
2016-01-23 22:14:56 -0500

    i386: Save integer-passed arguments to the stack, to aid debuggers.
    Merged from 5.3 to 7.2 by: Aurélien Larcher
    
    Originally implemented in:
        commit 023cc9a4c9c698bed1f51d38eac850d327fc1146
        Author: jsm28 <jsm28@138bc75d-0d04-0410-961f-82ee72b054a4>
        Date:   Wed Jun 29 23:51:34 2005 +0000
    
            	* gcc/dwarf2.h (DW_AT_SUN_amd64_parmdump): New.
            	* gcc/dwarf2out.c (gen_subprogram_die): Add this attribute.
            	* gcc/doc/invoke.texi (-msave-args): New x86-64 option.
            	* gcc/config/i386/i386.h (MASK_SAVE_ARGS, TARGET_SAVE_ARGS): New.
            	(TARGET_SWITCHES): Add -msave-args.
            	* gcc/config/i386/i386.c (struct ix86_frame): Add nmsave_args and
            	padding0.
            	(pro_epilogue_adjust_stack): Declare.
            	(ix86_nsaved_args): New.
            	(override_options, ix86_can_use_return_insn_p,
            	ix86_frame_pointer_required, ix86_compute_frame_layout,
            	ix86_emit_save_regs, ix86_emit_save_regs_using_mov,
            	ix86_expand_prologue, ix86_expand_epilogue): Handle -msave-args.
    
            git-svn-id: svn+ssh://gcc.gnu.org/svn/gcc/branches/csl-sol210-3_4-branch@101443 138bc75d-0d04-0410-961f-82ee72b054a4

--- gcc/config/i386/i386.h
+++ gcc/config/i386/i386.h
@@ -587,7 +587,7 @@ extern tree x86_mfence;
 #define TARGET_SUBTARGET64_DEFAULT 0
 #define TARGET_SUBTARGET64_ISA_DEFAULT 0
 
-/* Replace MACH-O, ifdefs by in-line tests, where possible. 
+/* Replace MACH-O, ifdefs by in-line tests, where possible.
    (a) Macros defined in config/i386/darwin.h  */
 #define TARGET_MACHO 0
 #define TARGET_MACHO_BRANCH_ISLANDS 0
@@ -1616,11 +1616,11 @@ enum reg_class
 /* If defined, the maximum amount of space required for outgoing arguments
    will be computed and placed into the variable `crtl->outgoing_args_size'.
    No space will be pushed onto the stack for each call; instead, the
-   function prologue should increase the stack frame size by this amount.  
+   function prologue should increase the stack frame size by this amount.
 
    In 32bit mode enabling argument accumulation results in about 5% code size
    growth because move instructions are less compact than push.  In 64bit
-   mode the difference is less drastic but visible.  
+   mode the difference is less drastic but visible.
 
    FIXME: Unlike earlier implementations, the size of unwind info seems to
    actually grow with accumulation.  Is that because accumulated args
@@ -2310,7 +2310,7 @@ do {							\
 } while (0)
 
 /* Which processor to tune code generation for.  These must be in sync
-   with processor_target_table in i386.c.  */ 
+   with processor_target_table in i386.c.  */
 
 enum processor_type
 {
@@ -2463,6 +2463,11 @@ enum avx_u128_state
 
    saved frame pointer			if frame_pointer_needed
 					<- HARD_FRAME_POINTER
+
+   [-msave-args]			<- arg_save_offset
+
+   [saveargs padding]
+
    [saved regs]
 					<- regs_save_offset
    [padding0]
@@ -2480,6 +2485,7 @@ enum avx_u128_state
   */
 struct GTY(()) ix86_frame
 {
+  int nmsave_args;
   int nsseregs;
   int nregs;
   int va_arg_size;
@@ -2491,6 +2497,7 @@ struct GTY(()) ix86_frame
   HOST_WIDE_INT hard_frame_pointer_offset;
   HOST_WIDE_INT stack_pointer_offset;
   HOST_WIDE_INT hfp_save_offset;
+  HOST_WIDE_INT arg_save_offset;
   HOST_WIDE_INT reg_save_offset;
   HOST_WIDE_INT sse_reg_save_offset;
 
