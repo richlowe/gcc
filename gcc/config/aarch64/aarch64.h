cb46791004a397abb6513c475258e2e8653ff0b3
Richard Lowe <richlowe@richlowe.net>
2022-10-05 15:32:52 -0500

    with the power of extreme violence, configure gcc correctly

--- gcc/config/aarch64/aarch64.h
+++ gcc/config/aarch64/aarch64.h
@@ -1458,7 +1458,8 @@ extern const char *aarch64_rewrite_mcpu (int argc, const char **argv);
    MCPU_TO_MARCH_SPEC
 
 #define EXTRA_SPECS						\
-  { "asm_cpu_spec",		ASM_CPU_SPEC }
+  { "asm_cpu_spec",		ASM_CPU_SPEC },		\
+  SUBTARGET_EXTRA_SPECS
 
 #define ASM_OUTPUT_POOL_EPILOGUE  aarch64_asm_output_pool_epilogue
 
